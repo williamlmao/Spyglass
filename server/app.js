d8101e667972c3e08a4d6d9940917c371dcae9b9
David <DavidMelnychuk@users.noreply.github.com>
2022-01-14 14:58:07 -0800

    use env file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/index.js");
+const dotenv = require("dotenv").config();
 
 const PORT = 4200;
-const MONGO_CONNECTION_URI = `mongodb+srv://admin:admin@cluster0.qbcla.mongodb.net/seernft?retryWrites=true&w=majority`;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const MONGO_CONNECTION_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.qbcla.mongodb.net/seernft?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGO_CONNECTION_URI, { useNewUrlParser: true }).then(() => {
   const app = express();
