00ded971f59ee0e8f95f84bad8795370013cf755
Will Liu <willliu@macbook-pro.lan>
2022-01-14 14:38:29 -0500

    fixing client

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,12 @@
+// Data from gsheets
+// // Available templates
+// // Currently selected template
+// // ...
+
+// // Data from user settings - isadmin is not admin
+
+// // Data about current workflow - creating new taxonomy, applied filters
+
+import { combineReducers } from "redux";
+
+export default combineReducers({});
